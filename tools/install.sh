067d6edd6e3609183be89a3376508b3f7542587a
Yevhenii Volosiuk <volosiukweb@gmail.com>
2025-01-04 03:19:32 +0100

    fix: installation errors

--- tools/install.sh
+++ tools/install.sh
@@ -28,7 +28,7 @@ command_exists() {
 # Log file for installation process
 install_log_file="$(mktemp)"
 
-os=$(uname -s) # Detect operating system
+OS=$(uname -s) # Detect operating system
 
 # Installer version
 installer_version="0.0.1"
@@ -56,17 +56,17 @@ warn() {
 
 show_kik_banner() {
   printf '\n'
-  printf '%s  ████╗  ████╗  ╔═█████═╗  ████╗  ████╗  %s\n' "$PURPLE" "$RESET_COLOR"
-  printf '%s  ████║  ████║  ║ █████ ║  ████║  ████║  %s\n' "$PURPLE" "$RESET_COLOR"
-  printf '%s  ████║ ████╔╝  ║ █████ ║  ████║ ████╔╝  %s\n' "$PURPLE" "$RESET_COLOR"
-  printf '%s  ████║ ████╔╝  ║ █████ ║  ████║ ████╔╝  %s\n' "$PURPLE" "$RESET_COLOR"
-  printf '%s  ██████╔╝      ║ █████ ║  █████╔╝       %s\n' "$PURPLE" "$RESET_COLOR"
-  printf '%s  ██████╔╝      ║ █████ ║  █████╔╝       %s\n' "$PURPLE" "$RESET_COLOR"
-  printf '%s  ████╔═████╗   ║ █████ ║  ████╔═████╗   %s\n' "$PURPLE" "$RESET_COLOR"
-  printf '%s  ████╔═████╗   ║ █████ ║  ████╔═████╗   %s\n' "$PURPLE" "$RESET_COLOR"
-  printf '%s  ████║  ████╗  ║ █████ ║  ████║  ████╗  %s\n' "$PURPLE" "$RESET_COLOR"
-  printf '%s  ████║  ████╗  ║ █████ ║  ████║  ████╗  %s\n' "$PURPLE" "$RESET_COLOR"
-  printf '%s  ╚═╝ ╚═╝  ╚═╝  ╚═╝╚═╝╚═╝  ╚═╝ ╚═╝  ╚═╝  %s\n' "$PURPLE" "$RESET_COLOR"
+  printf '%s  ████╗  ████╗  ╔═█████═╗  ████╗  ████╗  %s\n' "$PURPLE" "$NC"
+  printf '%s  ████║  ████║  ║ █████ ║  ████║  ████║  %s\n' "$PURPLE" "$NC"
+  printf '%s  ████║ ████╔╝  ║ █████ ║  ████║ ████╔╝  %s\n' "$PURPLE" "$NC"
+  printf '%s  ████║ ████╔╝  ║ █████ ║  ████║ ████╔╝  %s\n' "$PURPLE" "$NC"
+  printf '%s  ██████╔╝      ║ █████ ║  █████╔╝       %s\n' "$PURPLE" "$NC"
+  printf '%s  ██████╔╝      ║ █████ ║  █████╔╝       %s\n' "$PURPLE" "$NC"
+  printf '%s  ████╔═████╗   ║ █████ ║  ████╔═████╗   %s\n' "$PURPLE" "$NC"
+  printf '%s  ████╔═████╗   ║ █████ ║  ████╔═████╗   %s\n' "$PURPLE" "$NC"
+  printf '%s  ████║  ████╗  ║ █████ ║  ████║  ████╗  %s\n' "$PURPLE" "$NC"
+  printf '%s  ████║  ████╗  ║ █████ ║  ████║  ████╗  %s\n' "$PURPLE" "$NC"
+  printf '%s  ╚═╝ ╚═╝  ╚═╝  ╚═╝╚═╝╚═╝  ╚═╝ ╚═╝  ╚═╝  %s\n' "$PURPLE" "$NC"
   printf '\n'
 }
 
@@ -80,37 +80,68 @@ check_root() {
 # Check for dependencies and install them if needed
 install_dependency() {
   local dep="$1"
+
+
   if command -v "$dep" &> /dev/null; then
     success "$dep is already installed."
   else
     case "$OS" in
-    Linux)
-      warn "$dep is not installed. Attempting to install..."
-      if sudo apt-get update && sudo apt-get install -y "$dep"; then
-        success "$dep installed successfully."
+    Linux|Debian)
+      if command -v apt &> /dev/null; then
+       install_command="sudo apt update && sudo apt install -y"
+      elif command brew -v &> /dev/null; then
+       install_command="brew install"
+      elif command -v pacman &> /dev/null; then
+       install_command="sudo pacman -Sy --noconfirm"
+      elif command -v dnf &> /dev/null; then
+       install_command="sudo dnf install -y"
       else
-        error "Failed to install $dep. Please install it manually."
+       error "Could not determine package manager for $OS. Please install manually."
+       return 1
       fi
       ;;
     Darwin)
-      warn "$dep is not installed. Attempting to install..."
-      if brew install "$dep"; then
-        success "$dep installed successfully."
-      else
-        error "Failed to install $dep using Homebrew. Please install it manually."
+      if ! command -v brew &> /dev/null; then
+        error "Homebrew is not installed. Please install it from https://brew.sh/"
+        return 1
       fi
+      install_command="brew install"
       ;;
     *)
-      error "Unsupported operating system. Exiting."
+      error "Unsupported operating system: $OS. Please install manually."
       ;;
     esac
+
+    # Attempt installation
+    warn "$dep is not installed. Attempting to install..."
+    if eval "$install_command $dep"; then
+      success "$dep installed successfully."
+    else
+      error "Failed to install $dep. Please install it manually."
+      return 1
+    fi
+
   fi
+}
+
+check_gum_dependency(){
+    local gum_install_docs_url="https://github.com/charmbracelet/gum?tab=readme-ov-file#installation"
+
+    # Check if gum is installed
+    if ! command -v gum &> /dev/null; then
+      warn "The 'gum' tool is not installed. It's required for interactive prompts."
+      warn "To install, please visit the following URL ↓"
+      warn "$gum_install_docs_url"
+      warn "Some features might not be available without 'gum'."
+      exit 0;
+    fi
 }
 
 # Check for essential dependencies
 check_dependencies() {
   log "Checking dependencies..."
 
+  check_gum_dependency
   install_dependency docker
   install_dependency bash
   install_dependency curl
@@ -119,6 +150,19 @@ check_dependencies() {
 
   success "All dependencies are installed."
 }
+
+delete_existing_installation() {
+  if [[ -d "$kik_home_path" ]]; then
+    warn "Existing installation detected at $kik_home_path."
+    if rm -rf "$kik_home_path"; then
+      success "Existing installation deleted successfully."
+    else
+      error "Failed to delete existing installation. Exiting."
+      exit 1
+    fi
+  fi
+}
+
 
 # Backup existing installation
 backup_existing_installation() {
@@ -136,10 +180,11 @@ backup_existing_installation() {
 # Create necessary directories
 prepare_directories() {
   log "Preparing directories..."
-  backup_existing_installation
+
   mkdir -p "$kik_home_path/config"
   mkdir -p "$kik_home_path/logs"
-  success "Created necessary directories at $kik_home_path."
+
+  success "Created necessary directories at $kik_home_path"
 }
 
 # Create default .env and config files with optional customization
@@ -176,29 +221,60 @@ install_optional_components() {
   done
 }
 
+add_kik_path_to_user_shell() {
+  # --- Determine the user's shell ---
+  user_shell=$(basename "$SHELL")
+
+  # --- Check if ~/.local/bin is in PATH ---
+  if [[ ! ":$PATH:" =~ ":$HOME/.kik/bin:" ]]; then
+    warn "~/.kik/bin is not in your PATH. Kik may not be accessible globally."
+
+    # --- Prompt user for automatic PATH addition ---
+    read -r -p "Do you want to add it to your PATH automatically? [y/N] " response
+    case "$response" in
+      [yY][eE][sS]|[yY])
+        case "$user_shell" in
+          "bash" | "sh")
+            echo "export PATH=\"\$HOME/.kik/bin:\$PATH\"" >> "$HOME/.bashrc"
+            source "$HOME/.bashrc"
+            success "Path added to ~/.bashrc. You might need to open a new terminal or run 'source ~/.bashrc'."
+            ;;
+          "zsh")
+            echo "export PATH=\"\$HOME/.kik/bin:\$PATH\"" >> "$HOME/.zshrc"
+            source "$HOME/.zshrc"
+            success "Path added to ~/.zshrc. You might need to open a new terminal or run 'source ~/.zshrc'."
+            ;;
+          *)
+            warn "Unsupported shell. Please add the following line to your shell's configuration file manually:"
+            echo "export PATH=\"\$HOME/.kik/bin:\$PATH\""
+            ;;
+        esac
+        ;;
+      *)
+        # --- Provide manual instructions ---
+        warn "You can add it manually by running the following command:"
+        case "$user_shell" in
+          "bash" | "sh")
+            echo "  echo 'export PATH=\"\$HOME/.kik/bin:\$PATH\"' >> ~/.bashrc"
+            ;;
+          "zsh")
+            echo "  echo 'export PATH=\"\$HOME/.kik/bin:\$PATH\"' >> ~/.zshrc"
+            ;;
+          *)
+            echo "  Please consult your shell's documentation on how to modify the PATH environment variable."
+            ;;
+        esac
+        ;;
+    esac
+  else
+    success "~/.kik/bin is already in your PATH."
+  fi
+}
+
 # Make the CLI globally available by linking to /usr/local/bin
 make_cli_global() {
-  # --- Default installation path ---
-  local default_cli_path="/usr/local/bin/kik"
-  local cli_path="$default_cli_path"
-
-  # --- Check if a custom path is provided as an argument ---
-  if [[ $# -gt 0 ]]; then
-    cli_path="$1"
-  fi
-
-  # --- Prompt for installation path if not provided as argument ---
-  if [[ -z "$cli_path" ]]; then
-    # You can use Gum for a visual prompt here
-    read -r -p "Enter desired installation path (default: $default_cli_path): " cli_path
-    # If the user presses Enter without typing anything, use the default.
-    [[ -z "$cli_path" ]] && cli_path="$default_cli_path"
-  fi
-
-  # --- Validation: Ensure the provided path is a valid directory ---
-  if [[ ! -d "$(dirname "$cli_path")" ]]; then
-    error "Invalid installation path: $(dirname "$cli_path") is not a directory."
-  fi
+  # --- Set the installation path to the user's .kik directory ---
+  local cli_path="$HOME/.kik/kik" # Notice the 'kik' executable name at the end
 
   # --- Ensure the directory exists, create it if necessary ---
   mkdir -p "$(dirname "$cli_path")" || error "Failed to create directory: $(dirname "$cli_path")"
@@ -262,14 +338,19 @@ main() {
 
   check_dependencies
 
+#   backup_existing_installation
+  delete_existing_installation
+
   # --- Clone the repository ---
   git clone -b "$branch" "$remote" "$kik_home_path"  || error "Failed to clone repository."
   get_kik_version
 
   prepare_directories
   create_default_config
-  install_optional_components
+#   install_optional_components
   make_cli_global
+  add_kik_path_to_user_shell
+
   installation_summary
 }
 
